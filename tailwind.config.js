3b18a70268721bae426d26edf3fc47c63d54261b
Macrophs <santiagoanselmih3@gmail.com>
2023-09-02 03:49:15 -0400

    Merge feature/toNext -  Develop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-=======
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -30,4 +16,3 @@ module.exports = {
   },
   plugins: [],
 }
->>>>>>> feature/pagesToNext
